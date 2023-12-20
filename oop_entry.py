d0d76ee1128a97685b47ddca9d38c9df1d151d5c
Kamil Grześków <kamil.grzeskow7@gmail.com>
2023-12-20 09:16:06 +0100

    First changes in oop structure

--- oop_entry.py
+++ oop_entry.py
@@ -2,16 +2,25 @@ from glob import glob
 import xmltodict
 import csv
 import json
+from datetime import datetime
 
 
 class DataReader:
 
     @staticmethod
     def read_xml(file_path):
-        data = []
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(file_path, 'r') as file:
             data = xmltodict.parse(file.read())
-            data = data['users']['user']
+        data = data['users']['user']
+        for user in data:
+            children_list = []
+            if user['children'] is not None:
+                if type(user['children']['child']) == dict:
+                    children_list.append(user['children']['child'])
+                else:
+                    for child in user['children']['child']:
+                        children_list.append(child)
+            user['children'] = children_list
         return data
 
     @staticmethod
@@ -19,7 +28,16 @@ class DataReader:
         data = []
         with open(file_path, 'r') as file:
             reader = csv.DictReader(file, delimiter=';')
+
             for row in reader:
+                children = row['children'].split(',')
+                children_list = []
+                if children != ['']:
+                    for child in children:
+                        child_attrib = child.split(' ')
+                        child_dict = {'name': child_attrib[0], 'age': child_attrib[1].strip('()')}
+                        children_list.append(child_dict)
+                row['children'] = children_list
                 data.append(row)
         return data
 
@@ -27,7 +45,7 @@ class DataReader:
     def read_json(file_path):
         with open(file_path, 'r') as file:
             data = json.load(file)
-            return data
+        return data
 
 
 class Users:  # Creating a class and constructor for our users
@@ -77,17 +95,18 @@ class DataManager:
         for file_path in glob('InputData/**/*.json', recursive=True):
             self.users.append(DataReader.read_json(file_path))
 
+
+    def create_user_from_data(self, data):
+        pass
     def remove_duplicates(self):
         pass
 
 
 def main():
     data_manager = DataManager()
-    print(data_manager.users)
     data_manager.import_data()
     for i in data_manager.users:
         print(i)
-
 
 if __name__ == '__main__':
     main()
